e83d0c452f7dd24aae92a9fc7de9ae14a81c2679
JavonTeo <72335724+JavonTeo@users.noreply.github.com>
2021-05-05 19:33:09 +0800

    Update choosedealer.js

--- src/commands/choosedealer.js
+++ src/commands/choosedealer.js
@@ -1,24 +1,21 @@
 const fs = require('fs');
-let { gameApproval, playerList } = require('../bjWritestream.js');
 module.exports = {
     name: 'choosedealer',
     description: 'randomly chooses dealer from the playerList',
     execute(message)
     {
-        let dealerApproval = false;
-        if (gameApproval == false)
-        {
+        try {
+            let {gameApproval, playersList} = require('../bjWritestream.js');
+        } catch (err) {
+            console.log(err);
             message.channel.send('This command cannot be activated without launching a game ($startbj).');
-            return;
         }
-        else {
-            message.channel.send('Game is go.');
-        }
-        // const dealer_index = Math.floor(Math.random() * playerList.length) //comes out with random index from list
-        // dealer_user = playerList[dealer_index];
-        // message.channel.send(`${dealer_user.username}, you are this game's dealer.`);
-        // dealerApproval = true;
-        // const choosedealerarr = [dealerApproval, dealer_user];
-        // return choosedealerarr;
+        if (!gameApproval) { return; }
+        message.channel.send('Game is go.');
+        const dealerIndex = Math.floor(Math.random() * playersList.length) //comes out with random index from list
+        console.log(`dealerID = ${dealerID}`);
+        dealerID = playersList[dealerIndex];
+        fs.appendFileSync('../blackjackbot/src/bjWritestream.js', `\ndealerApproval = true;\ndealerID = ${dealerID}`);
+        bot.users.fetch()
     }
 }
