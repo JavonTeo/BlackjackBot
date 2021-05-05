893859e07e701e5665e671edbb4c444491b85b4d
JavonTeo <72335724+JavonTeo@users.noreply.github.com>
2021-05-05 19:32:38 +0800

    Update startbj.js

--- src/commands/startbj.js
+++ src/commands/startbj.js
@@ -1,11 +1,11 @@
 const { MessageCollector } = require('discord.js');
 const fs = require('fs');
-let { playersList, gameApproval } = require('../variables.js');
 module.exports = {
     name: "startbj",
     description: "kicks off the blackjack game",
     execute(message)
     {
+        let playersList = [];
         message.channel.send("Collecting players...reply with 'joinbj' in 10 seconds");
         const filter = (m) => m.content.toLowerCase() === "joinbj";
         const players_collector = new MessageCollector(message.channel, filter, {time: 10000});
@@ -51,27 +51,23 @@ module.exports = {
                 if (playersList.length > 1)     //collected.size OR playersList.length
                 {
                     resolve(message.channel.send(`${playersList.length} players received: ${playersList}. Reply '$choosedealer' for me to assign dealer (random).`));
-                    gameApproval = true;
                     let file = fs.createWriteStream('../blackjackbot/src/bjWritestream.js');
                     file.write('playersList = [');
                     playersList.forEach((sessPlayer) => {
                         file.write(sessPlayer + ', ');
                     })
                     file.write('];\n');
-                    file.write('gameApproval = true;');
+                    file.write('gameApproval = true;');     //gameApproval is declared here
                     file.end();
                 }
                 else
                 {
                     reject(message.channel.send(`Blackjack not launched due to insufficient players. Only ${playersList.length}, ${playersList.username} player joined.`));
-                    gameApproval = false;
                 }
             })
             launch_ornot
             .then((result) => {result;})
             .catch((result) => {result;});
         });
-        // const startbjarr =  [gameApproval, playersList];
-        // return gameApproval;
     },
 }
